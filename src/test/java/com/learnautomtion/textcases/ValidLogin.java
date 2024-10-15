886283a516e233cbdd2073e0bc355141ab36a4c4
Fouziya Banu <fouziya.banu@zucitech.com>
2024-10-15 17:46:45 +0530

    mess

---
+++ src/test/java/com/learnautomtion/textcases/ValidLogin.java
@@ -0,0 +1,17 @@
+package com.learnautomtion.textcases;
+
+import com.automation.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ValidLogin {
+    @Test
+            public void logging(){
+
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://test.salesforce.com/");
+        LoginPage l=new LoginPage(driver);
+        l.setLoginPage("avinash.r@zucitech.com.ztdev1","Zucitech@12");
+    }
+}
